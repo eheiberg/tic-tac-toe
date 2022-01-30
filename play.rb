2688b69c3c5db08c8a45565554072040611c09c2
Eric H <eric@mojonation.com>
2022-01-30 14:47:59 -0600

    a change  up to how we think of the game

--- play.rb
+++ play.rb
@@ -1,57 +1,97 @@
-class Board
-  @@board = nil
-  @@turn = 0
-
+class Player
   attr_reader :marker
 
-  class << self
-    def setup
-      row = [' ', ' ', ' ']
-      @@board = [
-        row.dup,
-        row.dup,
-        row.dup,
-      ]
-    end
-
-    def display
-      setup unless @@board
-      puts "========TURN #{@@turn }========="
-      @@board.each { |r| pp r }
-      puts '======================='
-    end
+  def initialize(marker, name=nil)
+    @marker = marker || 'X'
+    @name = name || "Player"
   end
 
-  def initialize(marker)
-    @marker = marker
+  def full_name
+    "#{@name} (#{@marker})"
+  end
+end
+
+
+class Game
+  MAXIMUM_MOVES_PER_GAME = 9
+
+  def initialize
+    @player1 = Player.new('X')
+    @player2 = Player.new('O')
+
+    pp "#{@player1.full_name} if player 1"
+    pp "#{@player2.full_name} if player 2"
+    @current_player = @player1
+    @turn_number = 1
+    row = [' ', ' ', ' ']
+    @board = [
+      row.dup,
+      row.dup,
+      row.dup,
+    ]
   end
 
-  def place(row, col)
-    if legal_move?(row, col)
-      @@turn += 1
-      @@board[row][col] = marker
-      self.class.display
-      return
+  def get_move
+    puts "#{@current_player.full_name}, it's your turn!"
+    legal_move_made = false
+    until legal_move_made
+      player_input = gets.chomp.split(/\s+/).map(&:to_i)
+      legal_move_made = legal_move?(*player_input)
+      unless legal_move_made
+        puts "Sorry, #{@current_player.full_name}, this is not a legal move! Try again."
+      end
     end
 
-    puts 'This space is already occupied! Try again.'
+    move(*player_input)
+  end
+
+  def print_board
+    puts "========TURN #{@turn_number }========="
+    @board.each { |row| pp row }
+    puts '======================='
+  end
+
+  def has_moves_left?
+    @turn_number <= MAXIMUM_MOVES_PER_GAME
   end
 
   private
 
+  def switch_current_player
+    @current_player = @current_player == @player1 ? @player2 : @player1
+  end
+
+  def move(row, col)
+    @board[row][col] = @current_player.marker
+    print_board
+    @turn_number += 1
+    switch_current_player
+  end
+
+  def space_unoccupied?(row, col)
+    @board[row][col].strip.empty?
+  rescue NoMethodError
+    return false
+  end
+
+  def space_exists?(row, col)
+    row <= 2 && col <= 2
+  end
+
   def legal_move?(row, col)
-    @@board[row][col].strip.empty?
+    space_unoccupied?(row, col) && space_exists?(row, col)
   end
 end
 
-# Begin play
-Board.setup
-p1 = Board.new('X')
-p2 = Board.new('O')
 
-p1.place(0, 0)
-p2.place(0, 1)
-p1.place(1, 1)
-p2.place(2, 2)
-p1.place(0, 1)
-p1.place(0, 2)
+def play_game
+  game = Game.new
+  game.print_board
+  while game.has_moves_left? do
+    game.get_move
+  end
+  puts "No more moves left. Thanks for playing!"
+end
+
+
+play_game
